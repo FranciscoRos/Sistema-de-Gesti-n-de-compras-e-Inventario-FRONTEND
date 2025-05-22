8b786cef615bc12b1dc71a7bf3b86c61fb71a14f
FranciscoRos <l21390377@chetumal.tecnm.mx>
2025-05-21 21:58:44 -0500

    Subiendo arquitectura lista

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+//Añadimos el bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+createApp(App).mount('#app')
