21378fe30dd6b806353c7dd93b9045f391ee9c16
FranciscoRos <l21390377@chetumal.tecnm.mx>
2025-05-29 10:29:31 -0500

    solo añadi el uso remoto

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
 export const API_BASE = 'http://localhost/Sistema-de-Gesti-n-de-compras-e-Inventario';
 
+//este es online
+//export const API_BASE = 'https://superelchino.com/apis/Sistema-de-Gesti-n-de-compras-e-Inventario/';
